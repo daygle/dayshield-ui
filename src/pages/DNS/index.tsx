71a891ef379d11d77729d882a168f30cb12edba1
daygle <daygle@daygle.net>
2026-05-21 15:12:17 +1000

    Interface/DHCP/PPPoE Fixes

--- src/pages/DNS/index.tsx
+++ src/pages/DNS/index.tsx
@@ -561,7 +561,7 @@ export default function DNS() {
               <path
                 strokeLinecap="round"
                 strokeLinejoin="round"
-                d="M15.232 5.232l3.536 3.536M9 11l6-6 3 3-6 6H9v-3z"
+                d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z"
               />
             </svg>
           </button>
@@ -618,7 +618,7 @@ export default function DNS() {
               <path
                 strokeLinecap="round"
                 strokeLinejoin="round"
-                d="M15.232 5.232l3.536 3.536M9 11l6-6 3 3-6 6H9v-3z"
+                d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z"
               />
             </svg>
           </button>
@@ -698,7 +698,7 @@ export default function DNS() {
               <path
                 strokeLinecap="round"
                 strokeLinejoin="round"
-                d="M15.232 5.232l3.536 3.536M9 11l6-6 3 3-6 6H9v-3z"
+                d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z"
               />
             </svg>
           </button>
