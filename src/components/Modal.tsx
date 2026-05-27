16ec9f9723fabdd859ccc5046f1e31f8eacb3f90
daygle <daygle@daygle.net>
2026-05-28 08:14:56 +1000

    Modal Fix

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -17,9 +17,9 @@ interface ModalProps {
 
 const sizeClass: Record<string, string> = {
   sm: 'max-w-sm',
-  md: 'max-w-lg',
-  lg: 'max-w-2xl',
-  xl: 'max-w-4xl',
+  md: '',
+  lg: '',
+  xl: '',
 };
 
 export default function Modal({
